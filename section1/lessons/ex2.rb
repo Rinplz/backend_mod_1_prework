b9df8fd61ecaef8749cc563bd77d14cea53120d6
Rin Fabian <pfabianschool@gmail.com>
2021-11-02 16:32:50 -0500

    example 3 complete

--- section1/lessons/ex2.rb
+++ section1/lessons/ex2.rb
@@ -1,7 +1,7 @@
 # A comment, this is so you can read your progam later.
-# Anything after the # will be ignored by ruby.
+# Anything after the # is ignored by ruby.
 
-puts "I could have code like this." # and the comment after will be ignored.
+puts "I could have code like this." # and the comment after is ignored.
 
 # You can also use a comment to "disable" or comment out a piece of code;
 # puts "This won't run."
---
+++ section1/lessons/ex3.rb
@@ -0,0 +1,28 @@
+# prints message
+puts "I will now count my chickens:"
+
+# + adds numbers together; / divides the number on the left of it by the one on the right
+puts "Hens #{25 + 30 / 6}"
+# - subtracts numbers; * multiplies numbers; % divides without displaying the remainder
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# < compares if the number on the left is lower in value than the one on the right
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "How about some more."
+
+# > compares if the number on the left is higher in value than the one on the right
+puts "Is it greater? #{5 > -2}"
+# >= compares if the number on the left is higher or equal in value to the number on the right
+puts "Is it greater or equal? #{5 >= -2}"
+# <= compares if the number on the left is lower or equal in value to the number on the right
+puts "Is it less or equal? #{5 <= -2}"
