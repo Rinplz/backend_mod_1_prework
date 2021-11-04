b19ff5c39e5878fe4ed457282f23f96548321cf3
Rin Fabian <pfabianschool@gmail.com>
2021-11-04 16:26:41 -0500

    complete example 7

---
+++ section1/lessons/ex7.rb
@@ -0,0 +1,39 @@
+#prints string on new line
+puts "Mary had a little lamb."
+#prints string on new line
+puts "It's fleece was white as #{'snow'}"
+#prints string on new line
+puts "And everywhere that Mary went."
+#prints string on new line
+puts "." * 10 #what'd that do? made the print repeat 10 times
+
+#creates end1 and defines it
+end1 = "C"
+#creates end2 and defines it
+end2 = "h"
+#created end3 and defines it
+end3 = "e"
+#creates end4 and defines it
+end4 = "e"
+#creates end5 and defines it
+end5 = "s"
+#creates end6 and defines it
+end6 = "e"
+#creates end7 and defines it
+end7 = "b"
+#creates end8 and defines it
+end8 = "u"
+#creates end9 and defines it
+end9 = "r"
+#creates end10 and defines it
+end10 = "g"
+#creates end11 and defines it
+end11 = "e"
+#creates end12 and defines it
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+#prints end1 end2 end3 end4 end5 and end6 next to each other
+print end1 + end2 + end3 + end4 + end5 + end6
+#prints end7 end8 end9 end10 end11 and end12 next to each other
+puts end7 + end8 + end9 + end10 + end11 + end12
