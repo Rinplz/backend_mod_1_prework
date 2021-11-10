df6c7a8bd3a945bfb19b04ba68bc3c35e0ce63a9
Rin Fabian <pfabianschool@gmail.com>
2021-11-10 13:46:19 -0600

    complete section 3

--- section3/exercises/hashes.rb
+++ section3/exercises/hashes.rb
@@ -1,31 +1,37 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section3/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
 foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
+puts '-' * 10
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {zebras: 10, monkeys: 6, seahorses: 20, lions: 7}
 p zoo
+puts '-' * 10
 
-# Write code that prints all of the 'keys' of the zoo variable 
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+puts zoo.keys
+puts '-' * 10
 
-# Write code that prints all of the 'values' of the zoo variable 
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+puts zoo.values
+puts '-' * 10
 
-# Write code that prints the value of the first animal of the zoo variable 
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
-# YOUR CODE HERE
-
-# Write code that adds an animal to the zoo hash. 
+puts zoo[:zebras]
+puts '-' * 10
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+zoo[:turtles] = 5
+puts zoo
+puts '-' * 10
 
 
 #-------------------
@@ -39,16 +45,26 @@ p zoo
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
+email = {
+  'sender' => 'user1',
+  'recipient' => 'user2',
+  'subject' => 'ruby is cool',
+  'timestamp' => '12:00',
+  'date' => 'Nov 10, 2021'
+}
+
 # Write code that prints your email hash to the terminal.
-
-
-# Write code that prints all of the 'keys' of the email hash 
+puts email
+puts '-' * 10
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
-# YOUR CODE HERE
+puts email.keys
+puts '-' * 10
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
-# YOUR CODE HERE
+puts email.values
+puts '-' * 10
 
 
 #-------------------
@@ -64,7 +80,7 @@ p zoo
 # posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 
-# Frankly, that was a very simplified version of the Array the Instagram developers have 
+# Frankly, that was a very simplified version of the Array the Instagram developers have
 # written and work with. Still probably slightly simplified as we don't know what their code
 # actually looks like, but it may look more like this:
 
@@ -76,7 +92,7 @@ posts = [
     'timestamp' => "4:37 PM August 13, 2019",
     'number_likes' => 0,
     'comments' => []
-  }, 
+  },
   {
     'image_src' => "./images/holiday-party.png",
     'caption' => "What a great holiday party omg",
@@ -90,12 +106,35 @@ puts posts
 puts posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in an 
-# Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in an
+# Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same
 # key-value pairs you used in your email Object above.
 # Then, log the email Array to the console.
+
+email_list = [
+  {
+    'sender' => 'user1',
+    'recipient' => 'user2',
+    'subject' => 'out of tp',
+    'timestamp' => '10:51 October 24, 2020'
+  },
+  {
+    'sender' => 'user1',
+    'recipient' => 'user3',
+    'subject' => 'Report questions',
+    'timestamp' => '3:54 October 27, 2020'
+  },
+  {
+    'sender' => 'user3',
+    'recipient' => 'user2',
+    'subject' => 'Halloween party attendance',
+    'timestamp' => '2:20 October 31, 2020'
+  }
+]
+
+puts email_list
--- section3/reflection.md
+++ section3/reflection.md
@@ -1,17 +1,37 @@
 ## Section 3 Reflection
 
 1. What are two points from the Growth Mindset article and/or video that either resonated with you, or were brand new to you?
+I wouldn't say anything mentioned was really brand new, but the open mindedness in the article
+resonated well with me since I try to be open minded in everything I do. Focusing on the process of
+reflection also resonated with me since just in this section I had used some brainstorming
+with experience from other syntax to make multiple additions to an array with one command.
 
 1. In which ways do you currently demonstrate a Growth Mindset? In which ways do you _not_?
+Being open minded and understanding that practice is essential for success are something
+I've known for quite a while in everything I've done.  Unfortunately, I've always taken failure
+particularly harshly so although I know making mistakes is part of the process of improvement,
+I sometimes get hung up on feeling dumb for not knowing something as fast as I think I should
 
 1. What is a Hash, and how is it different from an Array?
+A Hash is a way to store data similar to an Array, but is not ordered numerically,
+instead linking values to keys
 
 1. In the space below, create a Hash stored to a variable named `pet_store`.  This hash should hold an inventory of items and the number of that item that you might find at a pet store.
+pet_store = {
+  'bags_dog_food' => 15,
+  'bags_cat_food' => 13,
+  'chew_toys' => 34,
+}
 
 1. Given the following `states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}`, how would you access the value `"Iowa"`?
+states['Iowa']
 
 1. With the same hash above, how would we get all the keys?  How about all the values?
-
+states.keys for keys; states.values for values
 1. What is another example of when we might use a hash?  In your example, why is a hash better than an array?
+Different classes for your character in an RPG would be good to use a has for since you
+could list the different stats associated with each class such as 'STR' => 35, 'WIS' => 21
 
 1. What questions do you still have about hashes?
+I wouldn't say it's a question about hashes, but I'd like to see more hashes in published programs to
+get some kind of experience looking at professional use
