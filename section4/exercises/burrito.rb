e33b0d55af9461ae6afbcdc28fa32f4c983e9ae8
Rin Fabian <pfabianschool@gmail.com>
2021-11-11 15:31:29 -0600

    complete dog and burrito exercises

--- section4/exercises/burrito.rb
+++ section4/exercises/burrito.rb
@@ -1,4 +1,4 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
@@ -11,9 +11,59 @@ class Burrito
     @base     = base
     @toppings = toppings
   end
-end
+
+  def add_topping
+    available_toppings = ['cheese','salsa','guacamole','lettuce','sour cream']
+    held_top_index = available_toppings.index(available_toppings.sample())
+    held_topping = available_toppings[held_top_index]
+    counter = 0
+
+    until counter == available_toppings.length - 1
+      if held_topping == @toppings[counter]
+        available_toppings.delete_at(held_top_index)
+        held_top_index = available_toppings.index(available_toppings.sample())
+        held_topping = available_toppings[held_top_index]
+        counter = 0
+      end
+      counter += 1
+    end
+
+    p "Adding #{held_topping} to burrito..."
+    @toppings.push(held_topping)
+
+    @toppings
+
+  end
+
+  def remove_topping
+    toppings_remove = @toppings.index(@toppings.sample())
+    p "Removing #{@toppings[toppings_remove]} from burrito..."
+    @toppings.delete_at(toppings_remove)
+    @toppings
+
+  end
+
+  def change_protein
+    protein_choices = ['Chicken','Beans','Beef']
+    new_protein = protein_choices.sample()
+
+    if new_protein == @protein
+      until new_protein != @protein
+        new_protein =protein_choices.sample()
+      end
+    end
+
+    p "Changing #{@protein} to #{new_protein}..."
+    @protein = new_protein
+
+    end
+  end
+
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
+p dinner.change_protein
 p dinner.base
 p dinner.toppings
+p dinner.remove_topping
+p dinner.add_topping
